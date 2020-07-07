93340e0ef4de480607f887590900a157de5a68eb
lcgtest <67812994+lcgtest@users.noreply.github.com>
2020-07-07 21:35:28 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -19,3 +19,6 @@ with open(report_name, 'wb') as f:
     runner = HTMLTestRunner_PY3.HTMLTestRunner(f,verbosity=1,title="IHRM接口测试报告", description="这是人力资源管理系统的测试报告")
     # 使用runner运行测试套件
     runner.run(suite)
+    
+    
+print('hello world')
